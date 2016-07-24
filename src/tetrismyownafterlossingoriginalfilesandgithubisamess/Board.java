9c13bbb1360b9b5933999e1f52254d35384175c7
Justin LI <dzjustinli2@gmail.com>
2016-07-24 11:55:05 +1000

    Initial Commit

---
+++ src/tetrismyownafterlossingoriginalfilesandgithubisamess/Board.java
@@ -0,0 +1,341 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetrismyownafterlossingoriginalfilesandgithubisamess;
+
+/**
+ *
+ * @author justin
+ */
+import java.util.Arrays;
+
+/**
+ *
+ * @author justin
+ */
+class Board {
+
+    private final int WIDTH;
+    private final int HEIGHT;
+    private boolean[][] grid;
+    private int[] widthOfRows;
+    private int[] heightOfColumes;
+    /*TODO: where to initalise "maxHeight", where it is declared or where it is needed, or in the constructor? */
+    private int maxHeight;
+
+    //where should "backupWidthOfRows" and "backupHeightOfColumes" 
+    private boolean[][] backupGrid;
+    private int[] backupWidthOfRows;
+    private int[] backupHeightOfColumes;
+    private int backupMaxHeight;
+
+    private boolean committed = true;
+
+    private final boolean DEBUG = true;
+
+    Board(int widthOfBoard, int heightOfBoard) {
+        WIDTH = widthOfBoard;
+        HEIGHT = heightOfBoard;
+        grid = new boolean[widthOfBoard][heightOfBoard];
+
+        widthOfRows = new int[heightOfBoard];
+        heightOfColumes = new int[widthOfBoard];
+
+        //this is where "maxHeight" should be intialised, because when the "Board" object
+        //has just been intialised, the "maxHeight" should be 0 because no piece has been placed on the board
+        maxHeight = 0;
+
+        backupGrid = new boolean[widthOfBoard][heightOfBoard];
+        backupWidthOfRows = new int[heightOfBoard];
+        backupHeightOfColumes = new int[widthOfBoard];
+        backupMaxHeight = 0;
+
+    }
+
+    private static final int PLACE_OK = 0;
+    private static final int PLACE_ROW_FILLED = 1;
+    private static final int PLACE_OUT_OF_BOUNDS = 2;
+    private static final int PLACE_BAD = 3;
+
+    int getWIDTH() {
+        return WIDTH;
+    }
+
+    int getHEIGHT() {
+        return HEIGHT;
+    }
+
+    boolean[][] getGrid() {
+        return grid;
+    }
+
+    int[] getWidthOfRows() {
+        return widthOfRows;
+    }
+
+    int[] getHeightOfColumes() {
+        return heightOfColumes;
+    }
+
+    int getMaxHeight() {
+        return maxHeight;
+    }
+
+    int place(Piece placedPiece, int xCoordinateOnBoard, int yCoordinateOnBoard) {
+
+        if (committed == true) {
+            committed = false;
+        } else {
+            throw new RuntimeException("board is in the state of uncommited when the place method is called");
+        }
+
+        backup();
+
+        int result = PLACE_OK;
+        int placedPieceXCoordinate;
+        int placePieceYCoordinate;
+
+        TPoint[] body = placedPiece.getBody();
+        for (int i = 0; i < body.length; i++) {
+
+            placedPieceXCoordinate = xCoordinateOnBoard + body[i].x;
+            placePieceYCoordinate = yCoordinateOnBoard + body[i].y;
+
+            if (placedPieceXCoordinate < 0 || placePieceYCoordinate < 0
+                    || placedPieceXCoordinate >= WIDTH || placePieceYCoordinate >= HEIGHT) {
+                result = PLACE_OUT_OF_BOUNDS;
+
+                //TODO: what if instead of "break;", I used "return;" instead, does it make any difference? 
+                break;
+            }
+
+            if (grid[placedPieceXCoordinate][placePieceYCoordinate]) {
+                result = PLACE_BAD;
+
+                //TODO: what if instead of "break;", I used "return;" instead, does it make any difference? 
+                break;
+            }
+
+            grid[placedPieceXCoordinate][placePieceYCoordinate] = true;
+
+            if (heightOfColumes[placedPieceXCoordinate] < placePieceYCoordinate + 1) {
+                heightOfColumes[placedPieceXCoordinate] = placePieceYCoordinate + 1;
+            }
+
+            widthOfRows[placePieceYCoordinate]++;
+
+            if (widthOfRows[placePieceYCoordinate] == WIDTH) {
+                result = PLACE_ROW_FILLED;
+            }
+        }
+
+        //"computeMaxHeight()" is called every time the "int place(Piece placedPiece, int xCoordinateOnBoard, int yCoordinateOnBoard) {}
+        //is called, but instead of going through "heightOfColumes" every time, is there an more smarter, convineint and faster way 
+        //to recalculate "maxHeight"? Or calling "computeMaxHeight()" is good enough in these aspects .
+        computeMaxHeight();
+
+        sanityCheck();
+
+        return result;
+    }
+
+    private void backup() {
+        System.arraycopy(widthOfRows, 0, backupWidthOfRows, 0, HEIGHT);
+        System.arraycopy(heightOfColumes, 0, backupHeightOfColumes, 0, WIDTH);
+        for (int i = 0; i < grid.length; i++) {
+            System.arraycopy(grid[i], 0, backupGrid[i], 0, grid[i].length);
+        }
+        backupMaxHeight = maxHeight;
+    }
+
+    void clearRow() {
+
+        committed = false;
+
+        //"numberOfRowsCleared" is used recalculate "maxHeight",
+        //also, should I return the "numberOfRowscleared"? 
+        int numberOfRowsCleared = 0;
+        //is "hasFilledRow" redundant 
+        boolean hasFilledRow = false;
+        int rowTo, rowFrom;
+
+        // when "rowFrom < maxHeight", simply fill all rows above "rowTo" to false 
+        //note: "rowFrom" is index and starts from 0, "maxHeight" starts at 1
+        for (rowTo = 0, rowFrom = 1; rowFrom < maxHeight; rowTo++, rowFrom++) {
+            if (widthOfRows[rowTo] == WIDTH) {
+                //this if statment should only ever be executed once, i.e. when the first filled row
+                //is detected, which in turn means that "hasFilledRow" should still be false 
+                if (!hasFilledRow) {
+                    hasFilledRow = true;
+                    numberOfRowsCleared++;
+                }
+                while (widthOfRows[rowFrom] == WIDTH) {
+                    numberOfRowsCleared++;
+                    rowFrom++;
+                }
+            }
+            
+            while(hasFilledRow && widthOfRows[rowFrom] == WIDTH){
+                rowFrom++;
+            }
+            
+            if (hasFilledRow) {
+                copyRow(rowTo, rowFrom);
+            }
+        }
+        
+        fillEmptyRows(rowTo, rowFrom);
+        
+        
+        
+                // Rather than iterating through the grid every time to compute the heights
+        // we do the following.
+        // Case 1) Usually the height of every column decreases by the no of rows cleared
+        // Case 2) The exception to this happens when there is a gap below the cleared out row.
+        // Here we compute the height of the column using a for loop in O(n) time.
+        // Example
+        // ++++++  <-- row cleared out
+        // +++ ++  <-- gap in the 4th column makes updated height
+        // +++ ++  <-- of the 4th column using case 1 invalid
+        // + ++++
+        for(int i = 0; i < heightOfColumes.length; i++){
+            heightOfColumes[i] -= numberOfRowsCleared;
+            if(heightOfColumes[i] > 0 && !grid[i][heightOfColumes[i] - 1]){
+                heightOfColumes[i] = 0;
+                for(int j = 0; j < maxHeight; j++){
+                    if(grid[i][j]){
+                        heightOfColumes[i] = j + 1;
+                    }
+                }
+            }
+        }
+
+        //TODO: need to recompute "maxHeight" here, can simply use "computeMaxHeight()" method
+        //or come up with a quicker way to recompute "maxHeight" 
+        computeMaxHeight();
+
+        sanityCheck();
+    }
+
+    private int computeMaxHeight() {
+//        //TODO: initalise "maxHeight" here is too late, but where should it be initialised then
+        maxHeight = 0;
+
+        for (int i = 0; i < heightOfColumes.length; i++) {
+            if (maxHeight < heightOfColumes[i]) {
+                maxHeight = heightOfColumes[i];
+            }
+        }
+        return maxHeight;
+    }
+
+    private void copyRow(int rowTo, int rowFrom) {
+        for (int i = 0; i < WIDTH; i++) {
+            grid[i][rowTo] = grid[i][rowFrom];
+            widthOfRows[rowTo] = widthOfRows[rowFrom];
+        }
+    }
+
+    private void fillEmptyRows(int rowTo, int rowFrom) {
+        //"rowTo += 1" here because the original "rowTo" is copied from the original "rowFrom",
+        //therefore we need to add 1 to "rowTo" so we are filling rows all rows that are 
+        //previously occupied but is empty now 
+        for (; rowTo <= rowFrom; rowTo++) {
+            for (int i = 0; i < WIDTH; i++) {
+                grid[i][rowTo] = false;
+            }
+            widthOfRows[rowTo] = 0;
+        }
+    }
+
+    int dropHeight(Piece piece, int xCoordinateOfLowerLeftCornerOfThePieceOnBoard) {
+
+        int pieceDropStopHeight = 0;
+
+        int[] skirtOfPiece = piece.getSkirt();
+        for (int i = 0; i < skirtOfPiece.length; i++) {
+            int heightOfColumeAtXCoordinate = heightOfColumes[xCoordinateOfLowerLeftCornerOfThePieceOnBoard + i];
+            //add 1 to "heightOfColumeAtXCoordinate because the piece is going to stop one level above the 
+            //existing height of this colume 
+            int stopHeight = heightOfColumeAtXCoordinate + 1;
+            if (stopHeight > pieceDropStopHeight) {
+                pieceDropStopHeight = stopHeight;
+            }
+        }
+
+        return pieceDropStopHeight;
+    }
+
+//Wrong logic, but interesting to know why    
+//    int dropHeight(Piece piece, int xCoordinate){
+//        int dropStopHeight = 0;
+//        TPoint[] body = piece.getBody();
+//        for(int i = 0; i < body.length; i++){
+//            int xCoordinateOfTPointOnBoard = body[i].x + xCoordinate;
+//            int heightOfColumeAtXCoordinateAfterDrop = heightOfColumes[xCoordinateOfTPointOnBoard] + 1;
+//            if(heightOfColumeAtXCoordinateAfterDrop > dropStopHeight){
+//                dropStopHeight = heightOfColumeAtXCoordinateAfterDrop;
+//            }
+//        }
+//        return dropStopHeight;
+//    }
+//    
+    void undo() {
+        if (committed) {
+            return;
+        }
+
+        commit();
+
+        grid = backupGrid;
+        widthOfRows = backupWidthOfRows;
+        heightOfColumes = backupHeightOfColumes;
+        maxHeight = backupMaxHeight;
+
+        sanityCheck();
+
+    }
+
+    void commit() {
+        committed = true;
+    }
+
+    private void sanityCheck() {
+        if (!DEBUG) {
+            return;
+        }
+
+        int checkMaxHeight = 0;
+        int[] checkHeightOfColumes = new int[WIDTH];
+        int[] checkWidthOfRows = new int[HEIGHT];
+        //TODO: simply checking for the width of each row may not be enought,
+        //because it does not take into account where expected width and actual width of a row
+        //is the same but where this row is filled is different 
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                if (grid[i][j]) {
+                    checkWidthOfRows[j] += 1;
+                    checkHeightOfColumes[i] = j + 1;
+                }
+            }
+            if (checkHeightOfColumes[i] > checkMaxHeight) {
+                checkMaxHeight = checkHeightOfColumes[i];
+            }
+        }
+
+        if (!Arrays.equals(widthOfRows, checkWidthOfRows)) {
+            throw new RuntimeException("Actual width of row and checked width of rows does not match");
+        }
+
+        if (!Arrays.equals(heightOfColumes, checkHeightOfColumes)) {
+            throw new RuntimeException("Actual height of columes and checked height of columes does not match");
+        }
+
+        if (maxHeight != checkMaxHeight) {
+            throw new RuntimeException("Actual max height does not equal to checked max height");
+        }
+    }
+
+}
---
+++ test/tetrismyownafterlossingoriginalfilesandgithubisamess/BoardTest.java
@@ -0,0 +1,322 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetrismyownafterlossingoriginalfilesandgithubisamess;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author justin
+ */
+import java.util.Arrays;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author justin
+ */
+public class BoardTest {
+
+    Board board;
+    Piece pyramid;
+    Piece pyramid2;
+    Piece pyramid3;
+    Piece pyramid4;
+    Piece square;
+    Piece stick;
+    Piece s2;
+    Piece s22;
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        board = new Board(3, 6);
+        pyramid = new Piece(Piece.PYRAMID_STRING);
+        //should I make "computeNextRotation()" method in "Piece" class private? 
+        pyramid2 = pyramid.computeNextRotation();
+        pyramid3 = pyramid2.computeNextRotation();
+        pyramid4 = pyramid2.computeNextRotation().computeNextRotation();
+        square = new Piece(Piece.SQUARE_STRING);
+        stick = new Piece(Piece.STICK_STRING);
+        s2 = new Piece(Piece.S2_STRING);
+        s22 = s2.computeNextRotation();
+
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getWIDTH method, of class Board.
+     */
+    @Test
+    public void testGetWIDTH() {
+        System.out.println("getWIDTH");
+        int expResult = 3;
+        int result = board.getWIDTH();
+        assertEquals(expResult, result);
+        assertThat(result, is(equalTo(3)));
+
+    }
+
+    /**
+     * Test of getHEIGHT method, of class Board.
+     */
+    @Test
+    public void testGetHEIGHT() {
+        System.out.println("getHEIGHT");
+        int expResult = 6;
+        int result = board.getHEIGHT();
+        assertEquals(expResult, result);
+        assertThat(result, is(equalTo(6)));
+    }
+
+    /**
+     * Test of getGrid method, of class Board.
+     */
+    @Test
+    public void testGetGrid() {
+        System.out.println("getGrid");
+
+        board.place(pyramid, 0, 0);
+
+        boolean[][] expResult = {
+            {true, false, false, false, false, false},
+            {true, true, false, false, false, false},
+            {true, false, false, false, false, false}
+        };
+        boolean[][] result = board.getGrid();
+        assertTrue(Arrays.deepEquals(expResult, result));
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetGridAfterPlacingPyramidAndPyramid2() {
+        board.place(pyramid, 0, 0);
+        board.commit();
+        board.place(pyramid2, 1, 1);
+        boolean[][] result = board.getGrid();
+
+        boolean[][] expResult = {
+            {true, false, false, false, false, false},
+            {true, true, true, false, false, false},
+            {true, true, true, true, false, false}
+        };
+
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetGridAfterPlacingPyramidAndPyramid2AndSquare() {
+        board.place(pyramid, 0, 0);
+        board.commit();
+        board.place(pyramid4, 0, 1);
+        board.commit();
+        board.place(square, 1, 3);
+
+        boolean[][] result = board.getGrid();
+
+        boolean[][] expResult = {
+            {true, true, true, true, false, false},
+            {true, true, true, true, true, false},
+            {true, false, false, true, true, false}
+        };
+
+        assertArrayEquals(expResult, result);
+        assertTrue(Arrays.deepEquals(expResult, result));
+        assertThat(expResult, is(equalTo(result)));
+    }
+
+    /**
+     * Test of getWidthOfRows method, of class Board.
+     */
+    @Test
+    public void testGetWidthOfRows() {
+        System.out.println("getWidthOfRows");
+
+        board.place(pyramid, 0, 0);
+        board.commit();
+        board.place(pyramid4, 0, 1);
+        board.commit();
+        board.place(square, 1, 3);
+
+        int[] expResult = {3, 2, 2, 3, 2, 0};
+        int[] result = board.getWidthOfRows();
+
+        assertArrayEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of getHeightOfColumes method, of class Board.
+     */
+    @Test
+    public void testGetHeightOfColumes() {
+        System.out.println("getHeightOfColumes");
+
+        board.place(stick, 0, 0);
+        board.commit();
+        board.place(square, 1, 0);
+
+        int[] expResult = {4, 2, 2};
+        int[] result = board.getHeightOfColumes();
+
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of getMaxHeight method, of class Board.
+     */
+    @Test
+    public void testGetMaxHeight() {
+        System.out.println("getMaxHeight");
+
+        board.place(stick, 0, 0);
+        board.commit();
+        board.place(square, 1, 0);
+
+        int expResult = 4;
+        int result = board.getMaxHeight();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of place method, of class Board.
+     */
+    @Test
+    public void testPlace() {
+        System.out.println("place");
+        int result = board.place(pyramid, 1, 0);
+        assertEquals(2, result);
+
+        board.undo();
+
+        assertEquals(0, board.getMaxHeight());
+
+        int[] expectedWithOfRows = {0, 0, 0, 0, 0, 0};
+        int[] expectedHeightOfColumes = {0, 0, 0};
+
+        assertArrayEquals(expectedHeightOfColumes, board.getHeightOfColumes());
+        assertArrayEquals(expectedWithOfRows, board.getWidthOfRows());
+    }
+
+    /**
+     * Test of clearRow method, of class Board.
+     */
+    @Test
+    public void testClearRow() {
+        System.out.println("clearRow");
+
+        board.place(pyramid, 0, 0);
+
+        board.clearRow();
+
+        int[] expectedHeightOfColumes = {0, 1, 0};
+
+        assertArrayEquals(expectedHeightOfColumes, board.getHeightOfColumes());
+//        assertEquals(2, board.getMaxHeight());
+
+    }
+
+    @Test
+    public void testClearRowAfterPlacingPyramidAndPyramid3() {
+        board.place(pyramid, 0, 0);
+        board.commit();
+        board.place(pyramid3, 0, 2);
+        board.clearRow();
+
+        int[] expectedHeightOfColumes = {0, 2, 0};
+
+        assertArrayEquals(expectedHeightOfColumes, board.getHeightOfColumes());
+    }
+
+    @Test
+    public void testClearRowPyramidStickS2() {
+        board.place(pyramid, 0, 0);
+        board.commit();
+        board.place(stick, 0, 1);
+        board.commit();
+        board.place(s22, 1, 2);
+
+        board.clearRow();
+
+        boolean[][] expResult = {
+            {true, true, true, false, false, false},
+            {true, true, false, false, false, false},
+            {false, false, true, false, false, false}
+        };
+        
+        int[] expectedWithOfRows = { 2, 2, 2, 0, 0, 0};
+        int[] expectedHeightOfColumes = { 3, 2, 3};
+        
+        assertArrayEquals(expResult, board.getGrid());
+        assertTrue(Arrays.deepEquals(expResult, board.getGrid()));
+        
+        assertArrayEquals(expectedWithOfRows, board.getWidthOfRows());
+        assertArrayEquals(expectedHeightOfColumes, board.getHeightOfColumes()); 
+        assertEquals(3, board.getMaxHeight());
+    }
+
+    /**
+     * Test of dropHeight method, of class Board.
+     */
+    @Test
+    public void testDropHeight() {
+        System.out.println("dropHeight");
+        
+        board.place(pyramid, 0, 0);
+        board.commit();
+
+        int xCoordinateOfLowerLeftCornerOfThePieceOnBoard = 0;
+        
+        int result = board.dropHeight(pyramid3, xCoordinateOfLowerLeftCornerOfThePieceOnBoard);
+        
+        assertEquals(3, result);
+    }
+    
+    @Test
+    public void testDropHeight2(){
+        board.place(stick, 1, 0);
+        board.commit();
+        
+        int result = board.dropHeight(pyramid3, 0);
+        
+        assertEquals(5, result);
+    }
+
+    /**
+     * Test of undo method, of class Board.
+     */
+    @Test
+    public void testUndo() {
+        System.out.println("undo");
+        Board instance = null;
+        instance.undo();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
